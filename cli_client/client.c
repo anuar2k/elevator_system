e4e848b98f0da50a05ec99c66ff48afdf2fedc11
anuar2k <anuar2k@outlook.com>
2021-04-19 22:22:18 +0200

    client works

--- cli_client/client.c
+++ cli_client/client.c
@@ -2,14 +2,25 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ncurses.h>
 
 #include <elevator_system.h>
 #include <avsystem/commons/avs_vector.h>
 
 #define GREEN_ON_BLACK 1
+#define YELLOW_ON_BLACK 2
 
 #define WITH_ATTR(attr, fun, ...) attron(attr); fun(__VA_ARGS__); attroff(attr)
+#define BEG_X 40
+
+void rectangle_fill(int y, int x, int h, int w, char chr) {
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            mvaddch(y + i, x + j, chr);
+        }
+    }
+}
 
 int main() {
     printf("enter elevator count: ");
@@ -49,6 +60,7 @@ int main() {
     cbreak();
     noecho();
     curs_set(0);
+    keypad(stdscr, true);
 
     if (has_colors() == FALSE) {
         endwin();
@@ -59,10 +71,9 @@ int main() {
     use_default_colors();
     start_color();
     init_pair(GREEN_ON_BLACK, COLOR_GREEN, -1);
+    init_pair(YELLOW_ON_BLACK, COLOR_YELLOW, -1);
 
-    attron(COLOR_PAIR(GREEN_ON_BLACK));
-    mvprintw(0, 0, "ELEVATOR SYSTEM");
-    attroff(COLOR_PAIR(GREEN_ON_BLACK));
+    WITH_ATTR(COLOR_PAIR(GREEN_ON_BLACK), mvprintw, 0, 0, "ELEVATOR SYSTEM");
     mvprintw( 1, 0, "1. use "); WITH_ATTR(A_BOLD, printw, "ARROWS"); printw(" to select");
     mvprintw( 2, 0, "   floors/elevators");
     mvprintw( 3, 0, "2. use "); WITH_ATTR(A_BOLD, printw, "U"); printw(" to request a travel up");
@@ -74,22 +85,72 @@ int main() {
     mvprintw( 9, 0, "5. to advance, press "); WITH_ATTR(A_BOLD, printw, "SPACE"); printw(" (1 step)");
     mvprintw(10, 0, "   or keys "); WITH_ATTR(A_BOLD, printw, "1-9"); printw(" for 2^n steps");
     mvprintw(11, 0, "6. quit with "); WITH_ATTR(A_BOLD, printw, "Q");
+    mvprintw(13, 0, "step: ");
 
     elevator_system el_system;
     elevator_system_init(&el_system, elevator_count, floor_count);
 
     size_t curr_elevator = 0;
     size_t curr_floor = 0;
+    size_t curr_step = 0;
 
     while (true) {
-        //draw...
+        //draw elevator numbers
+        for (size_t i = 0; i < elevator_count; i++) {
+            if (i == curr_elevator) {
+                attron(COLOR_PAIR(YELLOW_ON_BLACK));
+                attron(A_BOLD);
+            }
+            mvprintw(0, BEG_X + 4 * i, "%3d", i);
+            if (i == curr_elevator) {
+                attroff(COLOR_PAIR(YELLOW_ON_BLACK));
+                attroff(A_BOLD);
+            }
+        }
+
+        //draw floor numbers
+        for (size_t i = 0; i < floor_count; i++) {
+            if (i == curr_floor) {
+                attron(COLOR_PAIR(YELLOW_ON_BLACK));
+                attron(A_BOLD);
+            }
+            mvprintw(2 * (floor_count - i), BEG_X - 3, "%3d", i);
+            if (i == curr_floor) {
+                attroff(COLOR_PAIR(YELLOW_ON_BLACK));
+                attroff(A_BOLD);
+            }
+        }
+
+        //clear elevator drawing
+        rectangle_fill(2, BEG_X + 2, 2 * floor_count - 1, 4 * elevator_count - 3, ' ');
+        
+        //write step
+        mvprintw(13, 6, "                      ");
+        mvprintw(13, 6, "%zu", curr_step);
+
+        for (size_t i = 0; i < elevator_count; i++) {
+            elevator *el = AVS_VECTOR_AT(el_system.elevators, i);
+            int x = BEG_X + 2 + 4 * i;
+            int y = 2 * (floor_count - el->last_floor);
+            if (el->state == EL_BETWEEN_FLOORS) {
+                y += el->direction == DIR_UP ? -1 : 1;
+            }
+            if (el->state == EL_STATIONARY) {
+                attron(COLOR_PAIR(GREEN_ON_BLACK));
+            }
+            mvaddch(y, x, '#');
+            if (el->state == EL_STATIONARY) {
+                attroff(COLOR_PAIR(GREEN_ON_BLACK));
+            }
+        }
+
         refresh();
         
-        //handle input...
+        //handle input
         int chr = getch();
         switch (chr) {
             case KEY_UP: {
-                if (curr_floor < AVS_VECTOR_SIZE(el_system.floors) - 1) {
+                if (curr_floor < floor_count - 1) {
                     curr_floor++;
                 }
             } break;
@@ -104,7 +165,7 @@ int main() {
                 }
             } break;
             case KEY_RIGHT: {
-                if (curr_elevator < AVS_VECTOR_SIZE(el_system.elevators) - 1) {
+                if (curr_elevator < elevator_count - 1) {
                     curr_elevator++;
                 }
             } break;
@@ -119,6 +180,7 @@ int main() {
             } break;
             case ' ': {
                 elevator_system_step(&el_system);
+                curr_step++;
             } break;
             case '1':
             case '2':
@@ -136,6 +198,7 @@ int main() {
                 }
                 while (steps-- > 0) {
                     elevator_system_step(&el_system);
+                    curr_step++;
                 }
             } break;
             case 'q': {
@@ -147,5 +210,6 @@ int main() {
     }
 
     quit:
+    elevator_system_free(&el_system);
     endwin();
 }
--- library/src/elevator_system_data.c
+++ library/src/elevator_system_data.c
@@ -3,5 +3,5 @@
 const size_t state_length[elevator_state_length] = {
     1, //EL_STATIONARY
     3, //EL_EN_ROUTE
-    2, //EL_PASSING_BY
+    3, //EL_PASSING_BY
 };
